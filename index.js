42f597b0e3d9742871b33c6f747eab160ad87be0
0x00ten <0x00ten@gmail.com>
2024-06-18 10:57:47 +0300

    import fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Tool1 from 'tools/tool1';
-import Tool2 from 'tools/tool2';
+import Tool1 from './tools/tool1/index.js';
+import Tool2 from './tools/tool2/index.js';
 
 export { Tool1, Tool2 };
