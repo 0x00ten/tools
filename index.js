af5a68e5199cde14ff3094e1d6c773cacf0d893d
0x00ten <0x00ten@gmail.com>
2024-06-18 13:43:19 +0300

    import fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Tool1 } = require('./tools/tool1/index.js');
-const { Tool2 } = require('./tools/tool2/index.js');
+const Tool1 = require('./tools/tool1/index.js');
+const Tool2 = require('./tools/tool2/index.js');
 
 module.exports = { Tool1, Tool2 };
